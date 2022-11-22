71d5ddb7bb854683c86bbe7e2e2258129d2260a6
vivify <vivify@vivifys-MacBook-Pro-9.local>
2022-11-22 19:24:59 +0100

    komit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    protected $fillable = ['title', 'body'];            //bez ovog se pravi greska, zbog zabrane da neko sa strane popunjava sta hoce od polja
     protected $table = 'post';
     public static function published(){
         return self::where('published', 1)->get();
